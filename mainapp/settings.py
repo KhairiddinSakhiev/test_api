2447a6bbb7e96d6c01823c5ba00291213228ab42
Khairiddin Sakhiev <93430063+KhairiddinSakhiev@users.noreply.github.com>
2023-12-22 19:29:53 +0500

    init 2

--- mainapp/settings.py
+++ mainapp/settings.py
@@ -83,14 +83,23 @@ WSGI_APPLICATION = 'mainapp.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'TaskManagerDb',
+#         'USER': 'postgres',
+#         'PASSWORD': 'Sakhi2000@',
+#         'HOST': 'localhost',
+#         'PORT': '5432',
+#     }
+# }
+
+
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'TaskManagerDb',
-        'USER': 'postgres',
-        'PASSWORD': 'Sakhi2000@',
-        'HOST': 'localhost',
-        'PORT': '5432',
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": "mydatabase",
     }
 }
 
